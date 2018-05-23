da30255eca2aa972ef49f35ba2553e360032ceba
ProphetFR <39571315+ProphetFR@users.noreply.github.com>
2018-05-24 00:18:02 +0200

    Update message.js

--- .gitignore/message.js
+++ .gitignore/message.js
@@ -1,9 +1,6 @@
 bot.on('message', message => {
   if (message.content === '!twitter') {
     message.reply('tu veux des info sur tes jeux pas de problaime les voila sur!')
-    message.reply('DOFUS https://twitter.com/DOFUSfr?lang=fr!')
-    message.reply('CS:GO https://twitter.com/csgo_dev?lang=fr!')
-    message.reply('Overwatch https://twitter.com/OverwatchFR?lang=fr!')
-    message.reply('fortnite https://twitter.com/FortniteGame?lang=fr!')
+    
   }
-});
+})
