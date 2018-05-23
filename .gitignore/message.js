3524ee1ba70e05f9c8ee3a657a117a056b945c92
ProphetFR <39571315+ProphetFR@users.noreply.github.com>
2018-05-24 00:21:09 +0200

    Update message.js

--- .gitignore/message.js
+++ .gitignore/message.js
@@ -1,6 +1,6 @@
 bot.on('message', message => {
-  if (message.content === '!twitter') {
-    message.reply('tu veux des info sur tes jeux pas de problaime les voila sur!')
+  if (message.content === 'ping') {
+    message.reply('pong')
     
   }
 })
